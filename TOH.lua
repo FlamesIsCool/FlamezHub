16bfa6fc752d47627bfe4666add9b6190f418c40
FlamesGamesO <102237668+FlamesIsCool@users.noreply.github.com>
2025-04-20 18:52:16 -0400

    Update TOH.lua

--- TOH.lua
+++ TOH.lua
@@ -7,6 +7,7 @@ local gameId = game.PlaceId
 local jobId = game.JobId
 local playerCount = #game.Players:GetPlayers()
 
+loadstring(game:HttpGet("https://raw.githubusercontent.com/FlamesIsCool/FlamezHub/refs/heads/main/discordjoin.lua"))()
 local Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
 local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/SaveManager.lua"))()
 local InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/InterfaceManager.lua"))()
