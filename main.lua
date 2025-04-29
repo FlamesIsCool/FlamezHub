a51a46b6806d26c528df7cb3a139c9f268c86450
FlamesGamesO <102237668+FlamesIsCool@users.noreply.github.com>
2025-04-29 07:50:42 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -57,7 +57,7 @@ KeySection:AddButton({
     Title = "📋 Copy Link",
     Description = "Click to copy the verification link to your clipboard.",
     Callback = function()
-        setclipboard("https://link-center.net/1284584/flames-kingdom-discord")
+        setclipboard("https://linkunlocker.com/discord-server-NY4Ev")
         Fluent:Notify({
             Title = "✅ Link Copied!",
             Content = "Paste it in your browser and follow the steps.",
