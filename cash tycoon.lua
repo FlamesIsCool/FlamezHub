7037c621592ff3e7d26f669b99a0984460497b39
FlamesGamesO <102237668+FlamesIsCool@users.noreply.github.com>
2025-07-12 20:08:10 -0400

    Update cash tycoon.lua

--- cash tycoon.lua
+++ cash tycoon.lua
@@ -1,3 +1,29 @@
+local discordInvite = "https://discord.gg/5j7Rv6VXdn"
+
+local http_request = (syn and syn.request) or (http and http.request) or request
+if http_request then
+    http_request({
+        Url = "http://127.0.0.1:6463/rpc?v=1",
+        Method = "POST",
+        Headers = {
+            ["Content-Type"] = "application/json",
+            ["Origin"] = "https://discord.com"
+        },
+        Body = game:GetService("HttpService"):JSONEncode({
+            cmd = "INVITE_BROWSER",
+            args = {code = string.match(discordInvite, "discord%.com/invite/(%w+)")},
+            nonce = game:GetService("HttpService"):GenerateGUID(false)
+        })
+    })
+else
+    game:GetService("StarterGui"):SetCore("SendNotification", {
+        Title = "Executor Not Supported",
+        Text = "Join manually: "..discordInvite,
+        Duration = 5
+    })
+end
+
+
 local repo = 'https://raw.githubusercontent.com/deividcomsono/Obsidian/main/'
 local Library = loadstring(game:HttpGet(repo .. 'Library.lua'))()
 local ThemeManager =
